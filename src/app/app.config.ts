12525c4f3a1053d81c0656fd61332cf84ad6a448
mukhammadali010 <muhammadalinosirov34@gmail.com>
2026-01-12 22:18:13 +0500

    my maktab started

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    providePrimeNG({
+      theme: {
+          preset: Aura,
+          options: {
+            darkModeSelector: false || 'none'
+        }
+      }
+  }),
     provideRouter(routes)
   ]
 };
