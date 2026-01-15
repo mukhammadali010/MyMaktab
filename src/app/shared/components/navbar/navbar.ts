2a445df53822e023f8495e0cf9641b7ff3458780
mukhammadali010 <muhammadalinosirov34@gmail.com>
2026-01-15 19:13:51 +0500

    home UI finished

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -5,50 +5,50 @@ import { Languages } from './navbar.model';
 import { ButtonComponent } from '../button/button';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.html',
-    imports: [Select, FormsModule, ButtonComponent],
+  selector: 'app-navbar',
+  templateUrl: './navbar.html',
+  imports: [Select, FormsModule, ButtonComponent],
 })
 export class Navbar implements OnInit {
-    languages: Languages[] | undefined;
-    selectedLangulage: Languages | undefined;
+  languages: Languages[] | undefined;
+  selectedLangulage: Languages | undefined;
 
-    navItems = [
-        { label: 'Loyiha haqida', target: 'about' },
-        { label: 'Xizmatlar', target: 'services' },
-        { label: 'Yangiliklar', target: 'news' },
-        { label: 'FAQ', target: 'faq' },
-        { label: 'Maktabni qidirish', target: 'hero' },
+  navItems = [
+    { label: 'Loyiha haqida', target: 'about' },
+    { label: 'Xizmatlar', target: 'services' },
+    { label: 'Yangiliklar', target: 'news' },
+    { label: 'FAQ', target: 'faq' },
+    { label: 'Maktabni qidirish', target: 'hero' },
+  ];
+
+  scrollTo(id: string) {
+    document.getElementById(id)?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  ngOnInit() {
+    this.languages = [
+      {
+        id: 1,
+        name: "O'z",
+        code: 'UZ',
+        flag: 'assets/flags/uz.svg',
+      },
+      {
+        id: 2,
+        name: 'Eng',
+        code: 'EN',
+        flag: 'assets/flags/en.svg',
+      },
+      {
+        id: 3,
+        name: 'Ru',
+        code: 'RU',
+        flag: 'assets/flags/ru.svg',
+      },
     ];
-
-    scrollTo(id: string) {
-        document.getElementById(id)?.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        });
-    }
-
-    ngOnInit() {
-        this.languages = [
-            {
-                id: 1,
-                name: "O'z",
-                code: 'UZ',
-                flag: 'assets/flags/uz.svg',
-            },
-            {
-                id: 2,
-                name: 'Eng',
-                code: 'EN',
-                flag: 'assets/flags/en.svg',
-            },
-            {
-                id: 3,
-                name: 'Ru',
-                code: 'RU',
-                flag: 'assets/flags/ru.svg',
-            },
-        ];
-        this.selectedLangulage = this.languages[0];
-    }
+    this.selectedLangulage = this.languages[0];
+  }
 }
